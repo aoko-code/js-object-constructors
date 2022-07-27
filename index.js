537af448864af827cee97553042957cc8c70ae20
aoko-code <cynthiakhamenge@yahoo.com>
2022-07-27 16:17:41 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function book(title, author, pages, read){
+
+function book(title, author, page, read){
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.page = page;
     this.read = read;
+    this.info = function(){
+        return book = `The ${this.title} by ${this.author}, ${page} pages , ${this.read} `
+    };
 }
-var book = new book('Miss Monet', 'Rodzina', 400, true)
+const book1 = new book('Miss Monet', 'perleka', 300, 'not read yet')
+
+
+
+
+
+// return book = new book(`The ${this.title} by ${this.author}, ${page} pages , ${this.read} `)
