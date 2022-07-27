c46761dd7f8854c35fe9b73500945f9d01c8f635
aoko-code <cynthiakhamenge@yahoo.com>
2022-07-27 14:54:05 +0300

    object constructor

---
+++ index.js
@@ -0,0 +1,7 @@
+function book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+var book = new book('Miss Monet', 'Rodzina', 400, true)
